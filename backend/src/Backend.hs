37cf9f406c79a85ba03428c2e90b54ba8f7399f2
Aaron Allen <aaron@flipstone.com>
2020-03-28 15:35:12 -0500

    adds filesystem based persistence

--- backend/src/Backend.hs
+++ backend/src/Backend.hs
@@ -1,20 +1,23 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
 module Backend
   ( backend
   ) where
 
 import           Common.Route
+import           Control.Concurrent (forkIO, threadDelay)
 import           Control.Concurrent.MVar (MVar, modifyMVar, modifyMVar_, newMVar)
 import           Control.Lens
-import           Control.Monad (forever)
-import           Control.Monad.Catch (finally)
+import           Control.Monad (forever, join)
+import           Control.Monad.Catch (finally, try)
 import qualified Data.Aeson as Aeson
 import qualified Data.ByteString as BS
 import           Data.Dependent.Sum
 import           Data.Foldable (traverse_)
 import qualified Data.Map as M
+import           Data.Maybe (fromMaybe)
 import qualified Network.WebSockets as WS
 import           Network.WebSockets.Snap (runWebSocketsSnap)
 import           Obelisk.Backend
@@ -30,6 +33,7 @@ data ServerState =
     { _ssClients       :: M.Map ClientId WS.Connection
     , _ssClientIdSeq   :: ClientId
     , _ssFrontendState :: Frontend.Model
+    , _ssPendingSave   :: Bool
     }
 
 makeLenses ''ServerState
@@ -37,12 +41,20 @@ makeLenses ''ServerState
 backend :: Backend BackendRoute FrontendRoute
 backend = Backend
   { _backend_run = \serve -> do
+      mbLoaded <- loadSavedFrontend
+
       stateMVar
         <- newMVar ServerState
-             { _ssClients = M.empty
-             , _ssClientIdSeq = ClientId 0
-             , _ssFrontendState = Frontend.initModel
+             { _ssClients       = M.empty
+             , _ssClientIdSeq   = ClientId 0
+             , _ssFrontendState = fromMaybe Frontend.initModel mbLoaded
+             , _ssPendingSave   = False
              }
+
+      -- Save the frontend to disk every 60 secs
+      _ <- forkIO . forever $ do
+        threadDelay 60000000
+        modifyMVar_ stateMVar saveFrontend
 
       serve $ \r ->
         case r of
@@ -90,7 +102,8 @@ talkToClient serverState clientId conn = do
   case Aeson.decodeStrict msg of
     Just ev -> modifyMVar_ serverState $ \s -> do
       let (newFrontend, newState) =
-            s & ssFrontendState <%~ Frontend.applyEvent ev
+            s & ssPendingSave %~ not
+              & ssFrontendState <%~ Frontend.applyEvent ev
 
       -- send full sync to client on key events
       if Frontend.isKeyEvent ev
@@ -110,4 +123,17 @@ broadcast :: ServerState -> ClientId -> BS.ByteString -> IO ()
 broadcast serverState senderId msg =
   let clients = M.delete senderId $ _ssClients serverState
    in traverse_ (flip WS.sendTextData msg) clients
+
+saveFrontend :: ServerState -> IO ServerState
+saveFrontend serverState = do
+  if _ssPendingSave serverState
+     then do
+       Aeson.encodeFile "saved-state.json" $ _ssFrontendState serverState
+       pure serverState { _ssPendingSave = False }
+     else pure serverState
+
+loadSavedFrontend :: IO (Maybe Frontend.Model)
+loadSavedFrontend = do
+  join . either (const Nothing) Just
+    <$> try @IO @IOError (Aeson.decodeFileStrict "saved-state.json")
 
